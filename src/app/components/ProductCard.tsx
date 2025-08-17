1b84d5903c1d9c96100165c65bc4e811dc8b8be0
Tiago Martins <97027227+relampago29@users.noreply.github.com>
2025-08-17 16:53:30 +0100

    feat: integrate Tailwind CSS and add new components
    - Updated package.json to include Tailwind CSS and PostCSS dependencies.
    - Modified globals.css to import Tailwind CSS.
    - Refactored Home component in page.tsx to include ProductCard and ShowImages components.
    - Created AddToCard component for adding items to the cart.
    - Added ProductCard component with styling.
    - Implemented ShowImages component to fetch and display random images from Unsplash.
    - Created NewUsersPage component for user creation.
    - Developed UsersPage component to fetch and display users from an API.

---
+++ src/app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCard from './AddToCard'
+
+
+const ProductCard = () => {
+  return (
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
+     <AddToCard/>
+    </div>
+  )
+}
+
+export default ProductCard
---
+++ src/app/users/new/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewUsersPage = () => {
+  return (
+    <div>
+      puta
+    </div>
+  )
+}
+
+export default NewUsersPage
+ 
