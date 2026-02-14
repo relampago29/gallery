e82523d977a8e6d988612ec96120376d53a21103
Relampago <rtiagomartins2005@gmail.com>
2026-02-14 19:51:39 +0000

    chore: remove debug console.log from i18n request

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -13,12 +13,6 @@ export default getRequestConfig(async ({ requestLocale }) => {
   try {
     // Usa o alias @ -> src/
     const messages = (await import(`@/locales/${chosen}/common.json`)).default;
-    console.log(
-      "[i18n] loaded messages for",
-      chosen,
-      "keys:",
-      Object.keys(messages)
-    );
     return { locale: chosen, messages };
   } catch (e) {
     console.error("[i18n] failed to load messages for", chosen, e);
